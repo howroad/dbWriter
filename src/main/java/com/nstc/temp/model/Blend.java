780e59cee806bb62ad2fa849e6892ee2eac30b91
howroad <kknn2@vip.qq.com>
2019-01-25 19:07:54 +0800

    新增测试用的

---
+++ src/main/java/com/nstc/temp/model/Blend.java
@@ -0,0 +1,40 @@
+package com.nstc.temp.model;
+
+import java.util.Date;
+
+/**  */
+public class Blend {
+    /** ���� */
+    private String id;
+    /** ȫ��Ψһ,ʹ��BLEND+���еķ�ʽʵ�� */
+    private String no;
+    /** ��wf_master_user���userId��Ӧ */
+    private Integer userId;
+    /** Yyyy-MM-dd 24HH:mm:ss */
+    private Date operateTime;
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+    public String getNo() {
+        return no;
+    }
+    public void setNo(String no) {
+        this.no = no;
+    }
+    public Integer getUserId() {
+        return userId;
+    }
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+    public Date getOperateTime() {
+        return operateTime;
+    }
+    public void setOperateTime(Date operateTime) {
+        this.operateTime = operateTime;
+    }
+    
+}
